6ef4afd2694931123a8666c14d70759b9dce2c72
Amiya Stillwell <stillwell.amiya@gmail.com>
2022-04-19 07:23:16 -0700

    completed all aspects of readme including description, known bugs, creators, license, setup/installation, and the link to the github repository.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
   $("#survey").submit(function(event) {
     event.preventDefault();
-    const name1
-    const name2
-    const born
-    const faveColor
+    const name1Input = $("input#name1").val(); 
+    const name2Input = $("input#name2").val();
+    const bornInpput = $("input#born").val();
+    const faveColorInput = $("input#faveColor").val();
+    const zodiacInput = $("input:radio[name=zodiac]:checked").val();
+    const drinkInput = $("#drink").val();
+    const animalsInput = $("#animals").val();
+    const  gemInput = $("#gem").val();
+
+    if (zodiacInput === #aries
+
+  });
+});
